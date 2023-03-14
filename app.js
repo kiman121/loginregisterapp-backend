0bf61f087a26ac044830ef0484958d05ce530519
Patrick <kimanthip@gmail.com>
2023-03-14 15:40:16 +0300

    Add access crontrol

--- app.js
+++ app.js
@@ -22,6 +22,16 @@ class App {
 
     this.app.use(cookieParser());
 
+    app.use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+      res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Content-Type,Authorization'
+      );
+      next();
+    });
+
     this.setRoutes();
   }
 
